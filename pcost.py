1f80a8087a58351601f1d5de6795a95c4c34c214
tiago <tiagosf99@gmail.com>
2023-09-18 15:37:22 +0100

    ex 1_4

--- pcost.py
+++ pcost.py
@@ -1,10 +1,17 @@
-total = 0.0
+def portfolio_cost(filename):
+    total = 0.0
 
-with open('Data/portfolio.dat', 'r') as f:
-    for line in f:
-        data = line.split()
-        n_shares = int(data[1])
-        price = float(data[2])
-        total += n_shares * price
+    with open(filename, 'r') as f:
+        for line in f:
+            try:
+                data = line.split()
+                n_shares = int(data[1])
+                price = float(data[2])
+                total += n_shares * price
+            except ValueError as e:
+                print(f"Couldn't parse: {line}\nReason: {e}")
+    
+    return total
 
-print(total)
+if __name__ == '__main__':
+    print(portfolio_cost('Data/portfolio3.dat'))
