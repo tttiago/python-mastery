9e1da8eb322cf4853ff67e573011981511ab70ff
tiago <tiagosf99@gmail.com>
2023-10-03 16:03:38 +0100

    ex3_1

--- stock.py
+++ stock.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Stock:
     def __init__(self, name, shares, price):
         self.name = name
@@ -6,3 +9,27 @@ class Stock:
 
     def cost(self):
         return self.shares * self.price
+
+    def sell(self, shares):
+        self.shares -= shares
+
+
+def read_portfolio(filename):
+    """Read a CSV file of stock data into a list of Stocks."""
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        portfolio = [Stock(row[0], int(row[1]), float(row[2])) for row in rows]
+    return portfolio
+
+
+def print_portfolio(portfolio):
+    print(f"{'name':>10} {'shares':>10} {'price':>10}")
+    print("-" * 10 + " " + "-" * 10 + " " + "-" * 10)
+    for s in portfolio:
+        print(f"{s.name:>10} {s.shares:10} {s.price:10.2f}")
+
+
+if __name__ == "__main__":
+    portfolio = read_portfolio("Data/portfolio.csv")
+    print_portfolio(portfolio)
