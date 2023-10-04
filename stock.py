3613bb1ce8f15bd7f307763c4ebed89cdc4455c3
tiago <tiagosf99@gmail.com>
2023-10-04 16:06:53 +0100

    ex3_6b

--- stock.py
+++ stock.py
@@ -10,6 +10,11 @@ class Stock:
     def __repr__(self):
         return f"{type(self).__name__}({self.name!r}, {self.shares!r}, {self.price!r})"
 
+    def __eq__(self, other):
+        return isinstance(other, Stock) and (
+            (self.name, self.shares, self.price) == (other.name, other.shares, other.price)
+        )
+
     @classmethod
     def from_row(cls, row):
         values = [func(val) for func, val in zip(cls._types, row)]
