9bf8d50048c75217f5435fe698786811658d06da
tiago <tiagosf99@gmail.com>
2023-10-05 16:13:24 +0100

    ex3_7c

--- reader.py
+++ reader.py
@@ -1,17 +1,44 @@
 import collections.abc
 import csv
+from abc import ABC, abstractmethod
+
+
+class CSVParser(ABC):
+    def parse(self, filename):
+        records = []
+        with open(filename) as f:
+            rows = csv.reader(f)
+            headers = next(rows)
+            for row in rows:
+                record = self.make_record(headers, row)
+                records.append(record)
+        return records
+
+    @abstractmethod
+    def make_record(self, headers, row):
+        pass
+
+
+class DictCSVParser(CSVParser):
+    def __init__(self, types):
+        self.types = types
+
+    def make_record(self, headers, row):
+        return {name: func(val) for name, func, val in zip(headers, self.types, row)}
+
+
+class InstanceCSVParser(CSVParser):
+    def __init__(self, cls):
+        self.cls = cls
+
+    def make_record(self, headers, row):
+        return self.cls.from_row(row)
 
 
 def read_csv_as_dicts(filename, coltypes):
     """Read a CSV file as a list of dictionaries with column type conversion."""
-    records = []
-    with open(filename, "r") as f:
-        rows = csv.reader(f)
-        headings = next(rows)  # Skip headers
-        for row in rows:
-            record = {name: func(val) for name, func, val in zip(headings, coltypes, row)}
-            records.append(record)
-    return records
+    parser = DictCSVParser(coltypes)
+    return parser.parse(filename)
 
 
 def read_csv_as_columns(filename, coltypes):
@@ -28,13 +55,8 @@ def read_csv_as_columns(filename, coltypes):
 
 def read_csv_as_instances(filename, cls):
     """Read a CSV file into a list of instances."""
-    records = []
-    with open(filename) as f:
-        rows = csv.reader(f)
-        headers = next(rows)
-        for row in rows:
-            records.append(cls.from_row(row))
-    return records
+    parser = InstanceCSVParser(cls)
+    return parser.parse(filename)
 
 
 class DataCollection(collections.abc.Sequence):
