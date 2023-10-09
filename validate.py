a0cfd98ab73422c09bea8f50dfe0958178a04e65
tiago <tiagosf99@gmail.com>
2023-10-09 11:52:55 +0100

    ex4_2c

--- validate.py
+++ validate.py
@@ -52,3 +52,51 @@ class PositiveFloat(Float, Positive):
 
 class NonEmptyString(String, NonEmpty):
     pass
+
+
+if __name__ == "__main__":
+
+    class Stock:
+        __slots__ = ("name", "_shares", "_price")
+        _types = (str, int, float)
+
+        def __init__(self, name, shares, price):
+            self.name = name
+            self.shares = shares
+            self.price = price
+
+        def __repr__(self):
+            return f"{type(self).__name__}({self.name!r}, {self.shares!r}, {self.price!r})"
+
+        def __eq__(self, other):
+            return isinstance(other, Stock) and (
+                (self.name, self.shares, self.price) == (other.name, other.shares, other.price)
+            )
+
+        @classmethod
+        def from_row(cls, row):
+            values = [func(val) for func, val in zip(cls._types, row)]
+            return cls(*values)
+
+        @property
+        def shares(self):
+            return self._shares
+
+        @shares.setter
+        def shares(self, value):
+            self._shares = PositiveInteger.check(value)
+
+        @property
+        def price(self):
+            return self._price
+
+        @price.setter
+        def price(self, value):
+            self._price = PositiveFloat.check(value)
+
+        @property
+        def cost(self):
+            return self.shares * self.price
+
+        def sell(self, nshares):
+            self.shares -= nshares
