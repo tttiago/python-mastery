662c5fad8dc724ee9b28db32663c7bc637f1ad10
tiago <tiagosf99@gmail.com>
2023-10-09 11:48:57 +0100

    ex4_2b

---
+++ validate.py
@@ -0,0 +1,54 @@
+class Validator:
+    @classmethod
+    def check(cls, value):
+        return value
+
+
+class Typed(Validator):
+    expected_type = object
+
+    @classmethod
+    def check(cls, value):
+        if not isinstance(value, cls.expected_type):
+            raise TypeError(f"Expected {cls.expected_type}")
+        return super().check(value)
+
+
+class Integer(Typed):
+    expected_type = int
+
+
+class Float(Typed):
+    expected_type = float
+
+
+class String(Typed):
+    expected_type = str
+
+
+class Positive(Validator):
+    @classmethod
+    def check(cls, value):
+        if value < 0:
+            raise ValueError("Expected >= 0")
+        return super().check(value)
+
+
+class NonEmpty(Validator):
+    @classmethod
+    def check(cls, value):
+        if len(value) == 0:
+            raise ValueError("Must be non-empty")
+        return super().check(value)
+
+
+class PositiveInteger(Integer, Positive):
+    pass
+
+
+class PositiveFloat(Float, Positive):
+    pass
+
+
+class NonEmptyString(String, NonEmpty):
+    pass
