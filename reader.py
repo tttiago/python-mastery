6a86fab41f770007930afd091f0d915637c82dd0
tiago <tiagosf99@gmail.com>
2024-01-16 18:40:07 +0000

    ex5_1b

--- reader.py
+++ reader.py
@@ -3,27 +3,41 @@ import csv
 
 def read_csv_as_dicts(filename, types):
     """
-    Read CSV data into a list of dictionaries with optional type conversion
+    Read CSV data into a list of dictionaries with optional type conversion.
     """
-    records = []
     with open(filename) as file:
-        rows = csv.reader(file)
-        headers = next(rows)
-        for row in rows:
-            record = {name: func(val) for name, func, val in zip(headers, types, row)}
-            records.append(record)
-    return records
+        return csv_as_dicts(file, types)
 
 
 def read_csv_as_instances(filename, cls):
     """
-    Read CSV data into a list of instances
+    Read CSV data into a list of instances.
+    """
+    with open(filename) as file:
+        return csv_as_instances(file, cls)
+
+
+def csv_as_dicts(lines, types):
+    """
+    Transform CSV lines into a list of dictionaries with optional type conversion.
     """
     records = []
-    with open(filename) as file:
-        rows = csv.reader(file)
-        headers = next(rows)
-        for row in rows:
-            record = cls.from_row(row)
-            records.append(record)
+    rows = csv.reader(lines)
+    headers = next(rows)
+    for row in rows:
+        record = {name: func(val) for name, func, val in zip(headers, types, row)}
+        records.append(record)
+    return records
+
+
+def csv_as_instances(lines, cls):
+    """
+    Transform CSV lines into a list of instances.
+    """
+    records = []
+    rows = csv.reader(lines)
+    headers = next(rows)
+    for row in rows:
+        record = cls.from_row(row)
+        records.append(record)
     return records
