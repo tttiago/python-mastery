aafa7bdb5f535037125813b766ef7c79e31149bf
tiago <tiagosf99@gmail.com>
2024-01-17 18:44:35 +0000

    ex5_2b

--- ex5_2.py
+++ ex5_2.py
@@ -1,3 +1,7 @@
 def parse_line(line):
     """Return both the name and value in a line of text of the form name=value."""
-    return tuple(line.split("="))
+    try:
+        name, value = line.split("=")
+        return name, value  # Return a tuple
+    except ValueError:
+        return None
