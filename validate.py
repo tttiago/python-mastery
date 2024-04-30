fe229cdb63d5a3da626dc7fd181c442d3ec40a05
tiago <tiagosf99@gmail.com>
2024-04-30 17:53:54 +0100

    ex6_5b

--- validate.py
+++ validate.py
@@ -1,3 +1,6 @@
+from inspect import signature
+
+
 class Validator:
     def __init__(self, name=None):
         self.name = name
@@ -61,6 +64,27 @@ class PositiveFloat(Float, Positive):
 
 class NonEmptyString(String, NonEmpty):
     pass
+
+
+class ValidatedFunction:
+    def __init__(self, func):
+        self.func = func
+        self.signature = signature(func)
+        self.annotations = dict(func.__annotations__)
+        self.retcheck = self.annotations.pop("return", None)
+
+    def __call__(self, *args, **kwargs):
+        bound = self.signature.bind(*args, **kwargs)
+
+        for name, val in self.annotations.items():
+            val.check(bound.arguments[name])
+
+        result = self.func(*args, **kwargs)
+
+        if self.retcheck:
+            self.retcheck.check(result)
+
+        return result
 
 
 if __name__ == "__main__":
