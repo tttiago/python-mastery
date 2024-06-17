d433f5054f18e362446cbae52c6be69c12070ac3
tiago <tiagosf99@gmail.com>
2024-06-17 16:30:36 +0100

    ex7_1a

---
+++ logcall.py
@@ -0,0 +1,8 @@
+def logged(func):
+    print("Adding logging to", func.__name__)
+
+    def wrapper(*args, **kwargs):
+        print("Calling", func.__name__)
+        return func(*args, **kwargs)
+
+    return wrapper
