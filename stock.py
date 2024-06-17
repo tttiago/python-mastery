39074afe48a4d6ae901d3b85b09cd587a35dc193
tiago <tiagosf99@gmail.com>
2024-06-17 18:07:29 +0100

    ex7_3a

--- stock.py
+++ stock.py
@@ -1,43 +1,12 @@
 from structure import Structure
+from validate import PositiveFloat, PositiveInteger, String
 
 
 class Stock(Structure):
     _fields = ("name", "shares", "price")
-    _types = (str, int, float)
-
-    def __eq__(self, other):
-        return isinstance(other, Stock) and (
-            (self.name, self.shares, self.price) == (other.name, other.shares, other.price)
-        )
-
-    @classmethod
-    def from_row(cls, row):
-        values = [func(val) for func, val in zip(cls._types, row)]
-        return cls(*values)
-
-    @property
-    def shares(self):
-        return self._shares
-
-    @shares.setter
-    def shares(self, value):
-        if not isinstance(value, self._types[1]):
-            raise TypeError(f"Expected a {self._types[1].__name__}")
-        if value < 0:
-            raise ValueError("shares must be >= 0")
-        self._shares = value
-
-    @property
-    def price(self):
-        return self._price
-
-    @price.setter
-    def price(self, value):
-        if not isinstance(value, self._types[2]):
-            raise TypeError(f"Expected a {self._types[2].__name__}")
-        if value < 0:
-            raise ValueError("price must be >= 0")
-        self._price = value
+    name = String()
+    shares = PositiveInteger()
+    price = PositiveFloat()
 
     @property
     def cost(self):
